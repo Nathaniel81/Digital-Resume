ac17057e545e31d0c5f4a61dea3f215a6e6b0b74
Nathaniel81 <nathanield356@gmail.com>
2023-11-25 21:08:56 +0300

    Add Index View

--- core/views.py
+++ core/views.py
@@ -1,3 +1,29 @@
+from typing import Any
 from django.shortcuts import render
+from django.contrib import messages
+from .models import (
+		Profile,
+		Blog,
+		Portfolio,
+		Testimonial,
+		Certificate
+	)
 
-# Create your views here.
+from django.views import generic
+
+class Index(generic.TemplateView):
+    template_name = 'core/index.html'
+    def get_context_data(self, **kwargs):    
+        context = super().get_context_data(**kwargs)
+
+        testimonials = Testimonial.objects.filter(is_active=True)
+        certificates = Certificate.objects.filter(is_active=True)
+        blogs = Blog.objects.filter(is_active=True)
+        portfolio = Portfolio.objects.filter(is_active=True)
+
+        context["testimonials"] = testimonials
+        context["certificates"] = certificates
+        context["blogs"] = blogs
+        context["portfolio"] = portfolio
+        print(context)
+        return context
