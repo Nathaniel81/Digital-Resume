4fb948ac76e390a321ca1b609fb640619e734007
Nathaniel81 <nathanield356@gmail.com>
2023-11-27 00:06:46 +0300

    Integrate Tailwind

--- core/views.py
+++ core/views.py
@@ -10,7 +10,7 @@ from django.views import generic
 from . forms import ContactForm
 
 class IndexView(generic.TemplateView):
-	template_name = "core/index.html"
+	template_name = "theme/base.html"
 
 	def get_context_data(self, **kwargs):
 		context = super().get_context_data(**kwargs)
--- resume/settings.py
+++ resume/settings.py
@@ -37,10 +37,23 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     'core',
     'ckeditor',
+    'tailwind',
+    'theme',
+    'django_browser_reload'
 ]
+
+TAILWIND_APP_NAME = 'theme'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+# NODE_PATH = os.path.join(BASE_DIR, )
+NPM_BIN_PATH = r'C:\Program Files\nodejs\npm.cmd'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -50,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = 'resume.urls'
